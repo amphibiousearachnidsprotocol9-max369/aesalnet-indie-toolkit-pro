81627f15129592fd36a5ba7e7b81b3fcfc926899
SALAMAN-DERMAN <amphibiousearachnidsprotocol9@gmail.com>
2026-01-29 11:14:49 -0800

    Add basic logging to main entrypoint

--- src/main.py
+++ src/main.py
@@ -1,4 +1,23 @@
 import argparse
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+def main():
+    parser = argparse.ArgumentParser(description="AESALNET Indie Toolkit")
+    parser.add_argument("--status", action="store_true", help="Show status")
+    args = parser.parse_args()
+
+    logging.info("Toolkit starting")
+
+    if args.status:
+        print("Toolkit status: ONLINE")
+    else:
+        print("AESALNET Indie Toolkit online")
+
+if __name__ == "__main__":
+    main()
+import argparse
 
 def main():
     parser = argparse.ArgumentParser(description="AESALNET Indie Toolkit")
