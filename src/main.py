86633e42b76c7384f5de7480f90a403a3e6dd330
SALAMAN-DERMAN <amphibiousearachnidsprotocol9@gmail.com>
2026-01-29 11:00:21 -0800

    Extend main entrypoint with status flag

--- src/main.py
+++ src/main.py
@@ -1,3 +1,17 @@
+import argparse
+
+def main():
+    parser = argparse.ArgumentParser(description="AESALNET Indie Toolkit")
+    parser.add_argument("--status", action="store_true", help="Show status")
+    args = parser.parse_args()
+
+    if args.status:
+        print("Toolkit status: ONLINE")
+    else:
+        print("AESALNET Indie Toolkit online")
+
+if __name__ == "__main__":
+    main()
 def main():
     print("AESALNET Indie Toolkit online")
 
